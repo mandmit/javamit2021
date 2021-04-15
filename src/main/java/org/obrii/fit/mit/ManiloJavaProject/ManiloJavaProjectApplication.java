fbcc319f2fc86fa267b6d00fd03ce1b91c0a6a4a
mandmit <manilodmitrij.02@gmail.com>
2021-04-15 03:28:48 +0300

    Final 1.0

---
+++ src/main/java/org/obrii/fit/mit/ManiloJavaProject/ManiloJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.obrii.fit.mit.ManiloJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiloJavaProjectApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(ManiloJavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/obrii/fit/mit/ManiloJavaProject/ManiloJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.obrii.fit.mit.ManiloJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiloJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
